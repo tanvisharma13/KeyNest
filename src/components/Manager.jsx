8b28433dbfe599cefa98b0d0f67a9ac971a86881
tanvisharma13 <tanvisharma1303@gmail.com>
2024-10-22 21:27:53 +0530

    Update local storage handling for saved passwords

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useRef, useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { v4 as uuidv4 } from 'uuid';
 
 const Manager = () => {
   const ref = useRef();
@@ -14,23 +17,56 @@ const Manager = () => {
     }
   }, []);
 
+  const copyText = (text) => {
+    toast("Copied to Clipboard!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+      transition: "Bounce",
+    });
+    navigator.clipboard.writeText(text);
+  };
+
   const showPassword = () => {
-    passwordRef.current.type="text"
+    passwordRef.current.type = "text";
     if (ref.current.src.includes("/slasheye.png")) {
-      ref.current.src = "/openeye.png"
-      passwordRef.current.type="password"
-      
+      ref.current.src = "/openeye.png";
+      passwordRef.current.type = "password";
     } else {
-      ref.current.src = "/slasheye.png"
-      passwordRef.current.type="text"
+      ref.current.src = "/slasheye.png";
+      passwordRef.current.type = "text";
     }
   };
 
   const savepassword = () => {
-    setPasswordArray([...passwordArray, form]);
-    localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));
+    setPasswordArray([...passwordArray, {...form, id: uuidv4()}]);
+    localStorage.setItem("passwords", JSON.stringify([...passwordArray,{...form, id: uuidv4()}]));
     console.log([...passwordArray, form]);
+    setform({ site: "", username: "", password: "" })
   };
+
+  const deletepassword = (id) => {
+    console.log("Deleting password with id", id)
+    let c= confirm("Do you really want to delete this password?")
+    if(c){
+    setPasswordArray(passwordArray.filter(item=>item.id!==id));
+   localStorage.setItem("passwords", JSON.stringify(passwordArray.filter(item=>item.id!==id)))
+   
+  }
+  };
+
+  const editpassword = (id) => {
+    console.log("Editing password with id", id)
+    setform(passwordArray.filter(i=>i.id===id)[0])
+    setPasswordArray(passwordArray.filter(item=>item.id!==id));
+ 
+  };
+
 
   const handleChange = (e) => {
     setform({ ...form, [e.target.name]: e.target.value });
@@ -38,6 +74,21 @@ const Manager = () => {
 
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition="Bounce"
+      />
+      {/* Same as */}
+      <ToastContainer />
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className=" mycontainer">
         <h1
@@ -73,7 +124,8 @@ const Manager = () => {
             />
 
             <div className="relative">
-              <input ref={passwordRef}
+              <input
+                ref={passwordRef}
                 value={form.password}
                 onChange={handleChange}
                 className="rounded-lg border border-blue-950 w-full p-4 py-1"
@@ -85,10 +137,7 @@ const Manager = () => {
               <span
                 className="absolute right-[3px] top-[4px] cursor-pointer"
                 onClick={showPassword}
-              > <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">  
-              <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
-              <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
-            </svg>
+              >
                 <img
                   ref={ref}
                   className="p-1"
@@ -110,36 +159,126 @@ const Manager = () => {
             >
               {" "}
             </lord-icon>
-            Add Password
+            Save Password
           </button>
         </div>
         <div className="password">
           <h2 className="font-bold text-2xl py-4">Your Password</h2>
-          {passwordArray.length === 0 && <div> No passwords to show  </div>}
-          {passwordArray.length != 0 && 
-          <table className="table-auto w-full rounded-md overflow-hidden">
-            <thead className="bg-blue-950 text-white">
-              <tr>
-                <th className="py-2">Site</th>
-                <th className="py-2">Username</th>
-                <th className="py-2">Password</th>
-              </tr>
-            </thead>
-            <tbody className="bg-blue-50">
-              {passwordArray.map((item,index) => {
-                return <tr key={index}>
-                <td className=" py-2 border border-white text-center w-32"><a href={item.site} target="_blank">{item.site} </a>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
-</svg>
-                </td>
-                <td className=" py-2 border border-white text-center w-32">{item.username}</td>
-                <td className=" py-2 border border-white text-center w-32">{item.password}</td>
-              </tr>
-            })}
-             
-            </tbody>
-          </table>}
+          {passwordArray.length === 0 && <div> No passwords to show </div>}
+          {passwordArray.length != 0 && (
+            <table className="table-auto w-full rounded-md overflow-hidden">
+              <thead className="bg-blue-950 text-white">
+                <tr>
+                  <th className="py-2">Site</th>
+                  <th className="py-2">Username</th>
+                  <th className="py-2">Password</th>
+                  <th className="py-2">Action</th>
+                </tr>
+              </thead>
+              <tbody className="bg-blue-50">
+                {passwordArray.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td className="  py-2 border border-white text-center ">
+                        <div className=" flex items-center justify-center ">
+                          <a href={item.site} target="_blank" className="p-3">
+                            {item.site}{" "}
+                          </a>
+                          <div
+                            className="size-4 cursor-pointer iconcopy "
+                            onClick={() => {
+                              copyText(item.site);
+                            }}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="currentColor"
+                              class="bi bi-copy"
+                              viewBox="0 0 16 16"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
+                              />
+                            </svg>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="  py-2 border border-white text-center ">
+                        <div className=" flex items-center justify-center ">
+                          <span className="p-3">{item.username} </span>
+                          <div
+                            className="size-4 cursor-pointer iconcopy "
+                            onClick={() => {
+                              copyText(item.username);
+                            }}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="currentColor"
+                              class="bi bi-copy"
+                              viewBox="0 0 16 16"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
+                              />
+                            </svg>
+                          </div>
+                        </div>
+                      </td>
+                      <td className=" py-2 border border-white text-center ">
+                        <div className=" flex items-center justify-center ">
+                          <span className="p-3"> {item.password}</span>
+                          <div
+                            className="size-4 cursor-pointer iconcopy "
+                            onClick={() => {
+                              copyText(item.password);
+                            }}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="currentColor"
+                              class="bi bi-copy"
+                              viewBox="0 0 16 16"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"
+                              />
+                            </svg>
+                          </div>
+                        </div>
+                      </td>
+                      <td className=" justify-center py-2  px-2 border border-white text-center ">
+                        <div className="flex justify-evenly">
+                        <span className="cursor-pointer" onClick={()=>{editpassword(item.id)}}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="19"
+                            height="19"
+                            fill="currentColor"
+                            class="bi bi-pen-fill"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001" />
+                          </svg>{" "}
+                        </span>
+                        <span className="cursor-pointer" onClick={()=>{deletepassword(item.id)}}>
+                          <lord-icon
+                            src="https://cdn.lordicon.com/skkahier.json"
+                            trigger="hover"
+                            style={{ width: "23px", height: "23px" }}
+                          ></lord-icon>
+                        </span>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
     </>
